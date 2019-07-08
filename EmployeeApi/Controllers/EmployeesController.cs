f26ddf69858d0d9fc7a87ff576602b675bbbb0d7
Liam Wheeler <liam.wheeler@kaleidaltd.co.uk>
2019-07-08 09:39:31 +0100

    Process employee data

--- EmployeeApi/Controllers/EmployeesController.cs
+++ EmployeeApi/Controllers/EmployeesController.cs
@@ -1,6 +1,8 @@
-﻿using KaleidaProject;
+﻿using EmployeeApi.Models;
+using KaleidaProject;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -13,11 +15,15 @@ namespace EmployeeApi.Controllers
     [Route("api/employees")]
     public class EmployeesController : Controller
     {
+        private static List<Employee> Employees = new List<Employee>();
+        private static readonly string DBPath = ConfigurationManager.AppSettings["CsvDatabasePath"];
+        private static EmployeeDataStore EmployeeData = new EmployeeDataStore();
 
         // GET api/values
         [HttpGet()]
         public List<Employee> GetEmployees(string path)
         {
+            Employees = EmployeeData.ProcessData(DBPath);
             return EmployeeDataStore.Current.ProcessData(path);
         }
 
