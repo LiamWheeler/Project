73369da7cf0b86d3c86b34186c06fd7e922a1359
Liam Wheeler <liam.wheeler@kaleidaltd.co.uk>
2019-07-08 15:02:56 +0100

    Get employes by town

--- EmployeeApi/Controllers/EmployeesController.cs
+++ EmployeeApi/Controllers/EmployeesController.cs
@@ -3,48 +3,72 @@ using KaleidaProject;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace EmployeeApi.Controllers
 {
-
+        [Route("api/employees")]
     public class EmployeesController : ApiController
     {
         private static EmployeeDataStore _dataStore = new EmployeeDataStore();
         List<Employee> employeeList = new List<Employee>();
         private static readonly string DBPath = ConfigurationManager.AppSettings["CsvDatabasePath"];
 
-        // GET api/values
+        // GET api/employees
         public List<Employee> GetEmployees()
         {
             return _dataStore.ProcessData(DBPath);           
         }
 
-        //// GET api/values/5
+        [HttpGet()]
+        // GET api/employees/id
         public IEnumerable<Employee> GetEmployee(int id)
         {
             return _dataStore.ProcessData(DBPath).Where(e => e.EmployeeId == id);
         }
 
-        //// POST api/values
-        public IEnumerable Post( [FromBody]string value)
+        [Route("api/employees/towns")]
+        // Get api/employees/employeesbytown
+        public List<string> GetEmployeesByTown()
         {
-            return value;
+            List<string> employeesByTown = new List<string>();
+            var Employees = _dataStore.ProcessData(DBPath);
+            var homeTownList = Employees.GroupBy(e => e.HomeTown)
+                               .OrderBy(e => e.Key)
+                               .Select(e => new
+                               {
+                                   HomeTown = e.Key,
+                                   numberOfEmployees = e.Count()
+                               });
+
+            foreach (var employee in homeTownList)
+            {
+                employeesByTown.Add(employee.ToString());
+            }
+
+            return employeesByTown;
         }
+
+
+        // POST api/values
+        //public IEnumerable Post( [FromBody]string value)
+        //{
+        //    return value;
+        //}
 
         //// PUT api/values/5
         //public void Put(int id, [FromBody]string value)
         //{
         //}
 
-        //// DELETE api/values/5
-        //public void Delete(int id)
+        // DELETE api/values/5
+        //public IEnumerable<Employee> DeleteEmployee(int id)
         //{
         //}
     }
